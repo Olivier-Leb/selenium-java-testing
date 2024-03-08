89d4e7f08f6aacf1303c93dd5dea653974785d94
Olivier <148598011+Olivier-Leb@users.noreply.github.com>
2024-03-08 11:52:50 -0500

    GitHub CI: modification of getConfigValue() to load property from environment variable first, + add definition of environment variables in GitHub action

--- src/main/java/example/selenium/utils/PropertyLoader.java
+++ src/main/java/example/selenium/utils/PropertyLoader.java
@@ -5,7 +5,24 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyLoader {
+
+    /**
+     * Return a property value based on environment variables or the 'config.properties' file.
+     * Ex: To have access to the property 'driver.default', either:
+     *   set an environment variable 'ENV_DRIVER_DEFAULT', or
+     *   set the value of 'driver.default' in resources/config.properties
+     *
+     * @param property Property name to find, if an environment variable of same name is defined, will be returned first
+     * @return Property value
+     */
     public static String getConfigValue(final String property) {
+        // Search in environment variable
+        String envValue = System.getenv("ENV_" + property.toUpperCase().replace(".", "_"));
+        if (envValue != null) {
+            return envValue;
+        }
+
+        // Fallback to the default .properties
         Properties properties = new Properties();
 
         try (InputStream propFileInpStream = PropertyLoader.class.getClassLoader()
@@ -13,9 +30,8 @@ public class PropertyLoader {
 
             properties.load(propFileInpStream);
 
-            String p = properties.getProperty(property);
-
-            return (p != null ? p : "");
+            String value = properties.getProperty(property);
+            return (value != null ? value : "");
         } catch (IOException e) {
             throw new RuntimeException("Failed to load configuration from config.properties");
         }
